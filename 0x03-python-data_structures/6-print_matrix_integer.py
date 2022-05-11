34ffc91c5374c4f176d8fb40f8c693a3dd369072
Alvaro <ajsabini@gmail.com>
2022-05-10 20:18:18 -0700

    ej 3-6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for elements in row:
+            print("{:d}".format(elements), end="")
+            if (row.index(elements) + 1) < len(row):
+                print(" ", end="")
+        print("\n", end="")
