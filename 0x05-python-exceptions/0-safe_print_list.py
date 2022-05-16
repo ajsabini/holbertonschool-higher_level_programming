f03d14f73b287d3d988fce92fb80413b81871538
Alvaro <ajsabini@gmail.com>
2022-05-16 16:13:32 -0700

    README

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     elems = 0
     for i in range(0, x):
         try:
-            print("{}".format(my_list[y]), end="")
+            print("{}".format(my_list[i]), end="")
             elems = elems + 1
         except Exception:
             break
