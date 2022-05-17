af78e0e617abf68ea9c3e165a7404f2d7f6d70a8
Alvaro <ajsabini@gmail.com>
2022-05-17 13:22:16 -0700

    0-3

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,13 +9,12 @@ def list_division(my_list_1, my_list_2, list_length):
             res = 0
             print("wrong type")
         except ZeroDivisionError:
-            res = 0
+            res = 0 
             print("division by 0")
         except IndexError:
             res = 0
-            print("out of range")
+            print("out of range")   
         finally:
-            div.append(res)
+        div.append(res)
     return div
-
 
---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Square:
+    """the class Square, empty"""
+    pass
