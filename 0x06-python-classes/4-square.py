d4b2f8fff76afe4b95fb623b551b1339620dc6dc
Alvaro <ajsabini@gmail.com>
2022-05-17 17:14:40 -0700

    ej 4-5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""create a class"""
+
+
+class Square:
+    """the class Square, that defines a square"""
+    def __init__(self, size=0):
+        """instantiation"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """public method"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """recover size(private) information"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
