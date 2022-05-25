24d895031061b5580b4206912315a214a360de26
Alvaro <ajsabini@gmail.com>
2022-05-25 08:22:48 -0700

    f 3

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,8 +5,14 @@ import unittest
 
 class TestingMaxInteger(unittest.TestCase):
 
-    def test_none(self):
+    def test_one_none(self):
         self.assertRaises(TypeError, max_integer, [None, 4, 6])
+
+    def test_all_none(self):
+        self.assertRaises(TypeError, max_integer, [None, None, None])
+
+    def test_one_str(self):
+        self.assertRaises(TypeError, max_integer, [23, "canfla", 9])
 
 if __name__ == '__main__':
     unittest.main()
