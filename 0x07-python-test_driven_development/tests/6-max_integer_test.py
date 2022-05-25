2f41dfb6d4f05a88dc04b9be1578abcb6126e252
Alvaro <ajsabini@gmail.com>
2022-05-25 10:18:44 -0700

    aa

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,8 +5,10 @@ import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestingMaxInteger(unittest.TestCase):
     """class test"""
+
     def test_one_none(self):
         self.assertRaises(TypeError, max_integer, [None, 4, 6])
 
@@ -15,24 +17,25 @@ class TestingMaxInteger(unittest.TestCase):
 
     def test_one_str(self):
         self.assertRaises(TypeError, max_integer, [23, "canfla", 9])
-    
+
     def test_max_str(self):
         self.assertEqual(max_integer['a', 'b', 'c'], 'c')
 
-    def test_bool(self)
+    def test_bool(self):
         self.assertRaise(TypeError, max_integer, [True, 2, 4])
 
-    def test_3_int(self)
+    def test_3_int(self):
         self.assertEqual(max_integer[1, 4, 5], 5)
 
-    def test_max_middle(self)
+    def test_max_middle(self):
         self.assertEqual(max_integer[4, 55, 9], 55)
 
-    def test_float(self)
+    def test_float(self):
         self.assertEqual(max_integer[22.3, 5, 7], 22)
 
-    def test_neg(self)
+    def test_neg(self):
         self.assertEqual(max_integer[-3, -22, -2], -2)
+
 
 if __name__ == '__main__':
     unittest.main()
