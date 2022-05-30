c3b083a0ddd7e466496714402d310551bc2efaad
Alvaro <ajsabini@gmail.com>
2022-05-30 15:58:04 -0700

    4

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
     is the object is an instance of or
-    if is an instance of a lass that 
+    if is an instance of a lass that
     inherited from
 """
 
 
 def is_kind_of_class(obj, a_class):
     """function that defines if an obj
-        is instance f or inst of 
+        is instance f or inst of
         inherited from
     """
 
