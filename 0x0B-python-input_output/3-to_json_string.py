465186ac47f7fb0a9feea3c240db7656d9eb2af0
Alvaro <ajsabini@gmail.com>
2022-05-31 13:48:13 -0700

    f 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""""""
+"""module - return json repr via obj"""
 
 
 import json
 
 
 def to_json_string(my_obj):
-    """"""
+    """function that repr obj to a json"""
     return json.dumps(my_obj)
