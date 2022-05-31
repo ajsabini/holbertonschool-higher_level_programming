32d6c198d08fd665ccf2d22ebfea49e3e0de825e
Alvaro <ajsabini@gmail.com>
2022-05-31 13:59:32 -0700

    fx 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""""""
+"""create an object from a json file"""
 
 
 import json
 
 
 def load_from_json_file(filename):
-    """"""
+    """funtion that do the onversion, recieve the json file"""
     with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
