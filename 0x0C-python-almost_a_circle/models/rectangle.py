fa0aeb78ec3a27ca489759e5c73840f7ff811740
ajsabini <ajsabini@gmail.com>
2022-06-03 08:47:36 -0700

    h5

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""module - rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """building object"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter __width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ setter __width """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """getter __height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """setter __height"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @property
+    def x(self):
+        """getter __x"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """setter __x"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+	
+    @property
+    def y(self):
+        """getter __y"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """setter __y"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
+    def area(self):
+        """calculate rectangle area"""
+        return self.__height * self.__width
+
+    def display(self):
+        """print in stdout the rect inst with #"""
