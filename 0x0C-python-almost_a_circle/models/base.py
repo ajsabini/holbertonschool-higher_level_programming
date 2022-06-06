79d31eb28e82ac625b4c64987c0278d7fa650063
ajsabini <ajsabini@gmail.com>
2022-06-06 08:22:24 -0700

    v

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
     def save_to_file(cls, list_objs):
         """write JSON string representation of list obj to a file"""
         obj_list = []
-        if list_objs is None
+        if list_objs is None:
             return my_list
         else:
             for obj in list_objs:
@@ -34,4 +34,32 @@ class Base:
             with open(cls.__name__ + ".json", 'w') as f:
                 f.write(cls.to_json_string(obj_list))
 
+    @staticmethod
+    def from_json_string(json_string):
+        """return json strin to dictionary obj"""
+        obj_list = []
+        if json_string is None:
+            return obj_list
+        return json.loads(json_string)
 
+
+    @classmethod
+    def create(cls, **dictionary):
+        """return an instance w/atribs already set"""
+        new_obj = cls(2, 4)
+        new_obj.update(**dictionary)
+        return new_obj
+
+    @classmethod
+    def load_from_file(cls):
+        """return a list of instances"""
+        my_dict = {}
+        my_list = []
+        try:
+            with open(cls.__name__ + ".json", "r") as f:
+                my_dict = json.load(f)
+        except Exception:
+            return my_list
+        for dictio in my_dict:
+            my_list.append(cls.create(**dictio))
+        return my_list
