6bbafa35d19e3ed1f80de45d7b9c9ea12343257a
Alvaro <ajsabini@gmail.com>
2022-06-13 19:39:29 -0700

    algunos 0*04

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def only_diff_elements(set_1, set_2):
+    new_set = []
+    for i in set_1:
+        if i not in set_2:
+            new_set.append(i)
+    for j in set_2:
+        if j not in set_1:
+            new_set.append(j)
+    return new_set
