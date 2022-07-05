aa134b7af7f5e75ec631f416cc035a5b338f42e9
Alvaro <ajsabini@gmail.com>
2022-07-05 11:55:12 -0700

    README +  0-2

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- script that create a database if not exists
+CREATE DATABASE if not exists hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- script that deletes a database, if dont exist shoul not fail
+DROP DATABASE if exists hbtn_0c_0
