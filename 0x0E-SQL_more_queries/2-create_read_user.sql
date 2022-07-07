dcce2da5031c0173640e1e1087b2672391a39718
Alvaro <ajsabini@gmail.com>
2022-07-07 15:37:25 -0700

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_2 an user user_0d_2
 CREATE DATABASE if not exists hbtn_0d_2;
 CREATE USER if not exists user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
-GRANT SELECT ON hbtn_0s_2.* TO 'user_0d_2'@'localhost';
+GRANT USAGE ON *.* TO `user_0d_2`@`localhost`;
+GRANT SELECT ON `hbtn_0s_2`.* TO `user_0d_2`@`localhost`;
 FLUSH PRIVILEGES;
