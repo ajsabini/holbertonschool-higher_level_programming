3b781123857debb74ac4f062413923f47962c590
Alvaro <ajsabini@gmail.com>
2022-07-06 20:19:32 -0700

    12-14

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
--- import database dump of hbtn_0d_tvshows
+-- script lists all shows contained in the database
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
