f64d790ca570332d92fd214237d1f973f07dfce0
ajsabini <ajsabini@gmail.com>
2022-08-01 10:28:09 -0700

    fix ss

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const lan of langs) {
+	console.log(lan);
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const cant = parseInt(process.argv[2],10);
+if (isNaN(num)) {
+
+}
