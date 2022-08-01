71681c32cebddadbee8b237e065d6fc8404ed6ae
ajsabini <ajsabini@gmail.com>
2022-08-01 10:57:23 -0700

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
-const cant = parseInt(process.argv[2],10);
-if (isNaN(num)) {
-
+const cant = parseInt(process.argv[2], 10);
+if (isNaN(cant)) {
+  console.log('Missing number of ocurrences');
+} else {
+  for (let i = cant; i > 0; i--) {
+    console.log('C is fun');
+  }
 }
