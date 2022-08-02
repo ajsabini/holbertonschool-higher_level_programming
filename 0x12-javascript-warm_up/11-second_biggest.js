b23076836b65e51a7cdfa2bf90e9318572da5763
ajsabini <ajsabini@gmail.com>
2022-08-01 17:58:29 -0700

    11-100

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   const numeros = process.argv.slice(2);
-  numeros.sort(function (a, b) {return b - a;});
+  numeros.sort(function (a, b) { return b - a; });
   console.log(numeros[1]);
 }
