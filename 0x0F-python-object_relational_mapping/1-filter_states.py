fb41a8c3842be486f8a66c428020e2da94c9f4cd
Alvaro <ajsabini@gmail.com>
2022-08-06 10:06:56 -0700

    md + 0-3

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" list states that star with N"""
+
+import MySQLdb
+import sys
+
+user_name = sys.argv[1]
+password = sys.argv[2]
+db_name = sys.argv[3]
+
+db = MySQLdb.connect(host='localhost', port=3306, user=user_name,
+                     passwd=password, db=db_name, charset='utf8')
+
+cursor = db.cursor()
+if (db):
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'" + 
+                    "ORDER BY states.id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+else:
+    print("could not connect")
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" takes in an argument and display all values """
+
+import MySQLdb
+import sys
+
+user_name = sys.argv[1]
+password = sys.argv[2]
+db_name = sys.argv[3]
+
+db = MySQLdb.connect(host='localhost', port=3306, user=user_name,
+                     passwd=password, db=db_name, charset='utf8')
+
+cursor = db.cursor()
+if (db):
+    cursor.execute("SELECT * FROM states WHERE name =%s ORDER BY states.id", (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+else:
+    print("could not connect")
