b2a10ffc77c9ebdc00b727b448c5a9f04f2efd9a
Alvaro <ajsabini@gmail.com>
2022-08-07 16:31:14 -0700

    f 6

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
     first state model, contains the class definition
     and an instane
 """
@@ -10,5 +10,7 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
