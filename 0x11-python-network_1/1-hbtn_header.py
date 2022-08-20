6953fa003bb761f85b8c3d66846aeebe37fa5fe6
ajsabini <ajsabini@gmail.com>
2022-08-20 02:04:43 +0000

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """ takes in a url, sends a request 
 to the url an display x-request-id  """
+
+
 from sys import argv
 import urllib.request
-
 
 if __name__ == "__main__":
 	req = urllib.request.Request(argv[1])
