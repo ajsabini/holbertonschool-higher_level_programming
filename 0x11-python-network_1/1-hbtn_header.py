7a44161c910069a6d0e1f26542919c1f377a3552
ajsabini <ajsabini@gmail.com>
2022-08-21 16:14:59 +0000

    f 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" takes in a url, sends a request 
-to the url an display x-request-id  """
+""" takes in a url, sends a request to the url an display x-request-id """
 
 
 from sys import argv
 import urllib.request
 
 if __name__ == "__main__":
-	req = urllib.request.Request(argv[1])
-	with urllib.request.urlopen(req) as response:
-		print(response.getheader('X-Request-Id'))
+
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader('X-Request-Id'))
