a13da00e5caae2c218e21827f52664636934c326
ajsabini <ajsabini@gmail.com>
2022-08-21 17:41:32 +0000

    f7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,8 +9,8 @@ from sys import argv
 
 if __name__ == "__main__":
 
-	req = requests.get(argv[1])
-	if req.status_code > 399:
-		print("Error code: {}".format(req.status_code))
-	else:
-		print(req.text)
+    req = requests.get(argv[1])
+    if req.status_code > 399:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
