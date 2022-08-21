ae87ee38a87bbce2788a3cbdb8cdf5d0373c077f
ajsabini <ajsabini@gmail.com>
2022-08-21 16:18:59 +0000

    f2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,9 +10,10 @@ import urllib.request
 import urllib.parse
 
 if __name__ == "__main__":
-	mail = {'email': argv[2]}
-	data = urllib.parse.urlencode(mail)
-	req = urllib.request.Request(argv[1], mail)
-	with urllib.request.urlopen(req) as response:
-		html = response.read()
-		print(html.decode('utf-8'))
+
+    mail = {'email': argv[2]}
+    data = parse.urlencode(mail)
+    req = request.Request(argv[1], mail)
+    with request.urlopen(req, data) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
