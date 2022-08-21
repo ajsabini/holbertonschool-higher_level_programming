6a1e4979d7e8b39661b2b71c7200e50931fe02c5
ajsabini <ajsabini@gmail.com>
2022-08-21 17:32:31 +0000

    f2-3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
+i!/usr/bin/python3
 """ takes in a url, sends a req to the url and
 displays body response, decode utf-8 - """
 
 
-import urllib.request
-from urllib.error import HTTPError
+from urllib import request, error
 from sys import argv
 
 if __name__ == "__main__":
-	try:
-		with urllib.request.urlopen(argv[1]) as response:
-			print(response.read().deode("utf-8", "replace"))
-	except HTTPError as e:
-		print("Error code: {}".fomrat(e.code))
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode("utf-8", "replace"))
+    except HTTPError as e:
+        print("Error code: {}".fomrat(e.code))
