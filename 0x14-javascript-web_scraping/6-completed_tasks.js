4263852c03dbd2b750d0db4f9f1574f3a3a10e81
ajsabini <ajsabini@gmail.com>
2022-09-12 20:39:46 -0700

    6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,32 +6,36 @@ let cuantos = 0;
 let cambioUserId = 0;
 
 const obtenerPelicula = async () => {
-  const respuesta = await axios.get(urlApi);
-  const diccRetorno = {};
+  try {
+    const respuesta = await axios.get(urlApi);
+    const diccRetorno = {};
 
-  // console.log(respuesta.data.length);
+    // console.log(respuesta.data.length);
 
-  for (let indice = 0; indice < respuesta.data.length; indice++) {
-    if (respuesta.data[indice].userId !== cambioUserId) {
-      if (cambioUserId !== 0) {
+    for (let indice = 0; indice < respuesta.data.length; indice++) {
+      if (respuesta.data[indice].userId !== cambioUserId) {
+        if (cambioUserId !== 0) {
         // console.log(cambioUserId + ' ' + cuantos);
-        diccRetorno[cambioUserId] = cuantos;
+          diccRetorno[cambioUserId] = cuantos;
+        }
+        cambioUserId = respuesta.data[indice].userId;
+        cuantos = 0;
       }
-      cambioUserId = respuesta.data[indice].userId;
-      cuantos = 0;
-    }
-    if (respuesta.data[indice].completed) {
-      cuantos++;
+      if (respuesta.data[indice].completed) {
+        cuantos++;
+      }
+
+      if (indice === respuesta.data.length - 1) {
+        if (respuesta.data[respuesta.data.length - 2].userId === respuesta.data[respuesta.data.length - 1].userId) {
+        // console.log(cambioUserId + ' ' + cuantos);
+          diccRetorno[cambioUserId] = cuantos;
+        }
+      }
     }
 
-    if (indice === respuesta.data.length - 1) {
-      if (respuesta.data[respuesta.data.length - 2].userId === respuesta.data[respuesta.data.length - 1].userId) {
-        // console.log(cambioUserId + ' ' + cuantos);
-        diccRetorno[cambioUserId] = cuantos;
-      }
-    }
+    console.log(diccRetorno);
+  } catch (error) {
+    console.log(error);
   }
-
-  console.log(diccRetorno);
 };
 obtenerPelicula();
