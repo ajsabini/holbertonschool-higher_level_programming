0e05c399f35225b370a92988f22ff86cf74df725
ajsabini <ajsabini@gmail.com>
2022-09-15 08:21:17 -0700

    f6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,37 +3,32 @@
 const urlApi = process.argv[2];
 const axios = require('axios');
 let cuantos = 0;
-let cambioUserId = 0;
+//let cambioUserId = 0;
 
 const obtenerPelicula = async () => {
   try {
     const respuesta = await axios.get(urlApi);
     const diccRetorno = {};
+	let val = 0;let valD = 0;
+	for (let indice = 0; indice < respuesta.data.length; indice++) {
+		if (respuesta.data[indice].userId in diccRetorno) {
+			
+			if (respuesta.data[indice].completed) {
+		
+				val = diccRetorno[respuesta.data[indice].userId].valueOf();
 
-    // console.log(respuesta.data.length);
-
-    for (let indice = 0; indice < respuesta.data.length; indice++) {
-      if (respuesta.data[indice].userId !== cambioUserId) {
-        if (cambioUserId !== 0) {
-        // console.log(cambioUserId + ' ' + cuantos);
-          diccRetorno[cambioUserId] = cuantos;
-        }
-        cambioUserId = respuesta.data[indice].userId;
-        cuantos = 0;
-      }
-      if (respuesta.data[indice].completed) {
-        cuantos++;
-      }
-      /*
-      if (indice === respuesta.data.length - 1) {
-        if (respuesta.data[respuesta.data.length - 2].userId === respuesta.data[respuesta.data.length - 1].userId) {
-        // console.log(cambioUserId + ' ' + cuantos);
-          diccRetorno[cambioUserId] = cuantos;
-        }
-      } */
-    }
-    diccRetorno[cambioUserId] = cuantos;
-    console.log(diccRetorno);
+		
+				val++;
+				diccRetorno[respuesta.data[indice].userId] = val;
+		
+			}
+		} else {
+		
+			diccRetorno[respuesta.data[indice].userId] = 1;
+		}
+				
+	}
+	  console.log(diccRetorno);
   } catch (error) {
     console.log(error);
   }
